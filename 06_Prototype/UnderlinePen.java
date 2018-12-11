c5ed6b0c08b66b2ee3ceea56cf75439e004af7bd
Sunghyun Kwon <sunghyun.kwon@smc.com>
2018-12-11 17:13:21 +0900

    Added the 06 Prototype design patter

---
+++ 06_Prototype/UnderlinePen.java
@@ -0,0 +1,29 @@
+import framework.*;
+
+public class UnderlinePen implements Product {
+    private char decochar;
+    public UnderlinePen(char decochar) {
+        this.decochar = decochar;
+    }
+
+    public void use(String s) {
+        int length = s.getBytes().length;
+        System.out.println("\"" + s + "\"");
+        System.out.print(" ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(decochar);
+        }
+        System.out.println(" ");
+    }
+
+    public Product createClone() {
+        Product p = null;
+        try {
+            p = (Product)clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return p;
+    }
+}
